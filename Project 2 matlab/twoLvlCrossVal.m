2a210fb6bbded9a977ced73991ecf62ef33516f6
LauritsFog <lause.fog@gmail.com>
2022-04-06 17:50:33 +0200

    Finished matlab

--- Project 2 matlab/twoLvlCrossVal.m
+++ Project 2 matlab/twoLvlCrossVal.m
@@ -26,7 +26,7 @@ K = 10;
 CV = cvpartition(N, 'Kfold', K);
 
 % Values of lambda
-lambda_tmp=10.^(-5:8);
+lambda_tmp=linspace(0.001,100,14);
 
 % Initialize variables
 T=length(lambda_tmp);
@@ -42,16 +42,18 @@ w_rlr = nan(M,K);
 mu = nan(K, M-1);
 sigma = nan(K, M-1);
 w_noreg = nan(M,K);
-lineRegEgens = zeros(K,T);
+Error_train_gen_rlr = zeros(K,T);
+Error_test_gen_rlr = zeros(K,T);
 
-NHiddenUnits = [1:10];
+NHiddenUnits = [1:3];
 Error_train_ANN = nan(K,1);
 Error_test_ANN = nan(K,1);
 Error_train2_ANN = nan(length(NHiddenUnits),K);
 Error_test2_ANN = nan(length(NHiddenUnits),K);
 bestnet=cell(K,1);
 h_opt = nan(K,1);
-ANNEgens = zeros(K,K);
+Error_train_gen_ANN = zeros(K,length(NHiddenUnits));
+Error_test_gen_ANN = zeros(K,length(NHiddenUnits));
 
 % For each crossvalidation fold
 for k = 1:K
@@ -90,8 +92,8 @@ for k = 1:K
             regularization(1,1) = 0; % Remove regularization of bias-term
             w(:,t,kk)=(XtX2+regularization)\Xty2;
             % Evaluate training and test performance
-            Error_train2(t,kk) = sum((y_train2-X_train2_std*w(:,t,kk)).^2)/CV2.TrainSize(kk);
-            Error_test2(t,kk) = sum((y_test2-X_test2_std*w(:,t,kk)).^2)/CV2.TestSize(kk);
+            Error_train2(t,kk) = sum((y_train2-X_train2_std*w(:,t,kk)).^2);
+            Error_test2(t,kk) = sum((y_test2-X_test2_std*w(:,t,kk)).^2);
         end
         
         MSEBest = inf;
@@ -105,17 +107,27 @@ for k = 1:K
             y_test_est = netwrk.t_pred_test;        
 
             % Compute least squares error
-            Error_train2_ANN(t,kk) = sum((y_train2-y_train_est).^2)/CV2.TrainSize(kk);
-            Error_test2_ANN(t,kk) = sum((y_test2-y_test_est).^2)/CV2.TestSize(kk); 
+            Error_train2_ANN(t,kk) = sum((y_train2-y_train_est).^2);
+            Error_test2_ANN(t,kk) = sum((y_test2-y_test_est).^2); 
         end
     end
     
-    for s = 1:length(NHiddenUnits)
-        ANNEgens(k,s) = Error_test2_ANN(s,:)*CV2.TestSize'/CV.TrainSize(k);
-    end
-    for s = 1:length(lambda_tmp)
-        lineRegEgens(k,s) = Error_test2(s,:)*CV2.TestSize'/CV.TrainSize(k);
-    end
+    Error_test_gen_ANN(k,:) = sum(Error_test2_ANN,2)/sum(CV2.TestSize);
+    Error_train_gen_ANN(k,:) = sum(Error_train2_ANN,2)/sum(CV2.TrainSize);
+    
+    Error_test_gen_rlr(k,:) = sum(Error_test2,2)/sum(CV2.TestSize);
+    Error_train_gen_rlr(k,:) = sum(Error_train2,2)/sum(CV2.TrainSize);
+    
+%     for s = 1:length(NHiddenUnits)
+%         Error_test_gen_ANN(k,s) = Error_test2_ANN(s,:)*CV2.TestSize'/CV.TrainSize(k);
+%         Error_train_gen_ANN(k,s) = Error_train2_ANN(s,:)*CV2.TrainSize'/CV.TrainSize(k);
+%     end
+%     for s = 1:length(lambda_tmp)
+%         Error_test_gen_rlr(k,s) = Error_test2(s,:)*CV2.TestSize'/CV.TrainSize(k);
+%         Error_train_gen_rlr(k,s) = Error_train2(s,:)*CV2.TrainSize'/CV.TrainSize(k);
+%     end
+    
+    % Training and testing 
     
     [val_ANN,ind_opt_ANN]=min(sum(Error_test2_ANN,2)/sum(CV2.TestSize));
     h_opt(k) = NHiddenUnits(ind_opt_ANN);
@@ -133,9 +145,6 @@ for k = 1:K
     [val,ind_opt]=min(sum(Error_test2,2)/sum(CV2.TestSize));
     lambda_opt(k)=lambda_tmp(ind_opt);  
     
-    % Standardize datasets in outer fold, and save the mean and standard
-    % deviations since they're part of the model (they would be needed for
-    % making new predictions)
     mu(k,  :) = mean(X_train(:,2:end));
     sigma(k, :) = std(X_train(:,2:end));
 
@@ -170,8 +179,13 @@ linRegEgen = sum(CV.TestSize*Error_test_rlr/N);
 ANNEgen = sum(CV.TestSize*Error_test_ANN/N);
 baselineEgen = sum(CV.TestSize*Error_test_nofeatures/N);
 
+%%
+
 figure
-bar([1,2,3],[linRegEgen,ANNEgen,baselineEgen])
+bar([linRegEgen,ANNEgen,baselineEgen])
+set(gca, 'XTickLabel',{'RLR','ANN','Baseline'},'Fontsize',15)
+title('Generalization errors')
+grid on
 
 %%
 
@@ -181,65 +195,61 @@ displayNetworkRegression(netwrkOuter);
 
 %%
 
-% Plotting training errors for every rlr model for each fold
-
 figure
-for i = 1:K
-    % subplot(5,2,i)
-    semilogx(lambda_tmp,Error_train2(:,i))
-    hold on
-end
+plot([1:10],Error_train_rlr,'-o','Color','b')
+hold on
+plot([1:10],Error_test_rlr,'-o','Color','r')
+legend('Training errors','Testing errors','Fontsize',15)
+grid on
+hold off
 
 %%
 
-% Plotting training errors for every ANN model for each fold
-
 figure
-for i = 1:K
-    % subplot(5,2,i)
-    plot(NHiddenUnits,Error_train2_ANN(:,i))
-    hold on
-end
+plot([1:10],Error_train_ANN,'-o','Color','b')
+hold on
+plot([1:10],Error_test_ANN,'-o','Color','r')
+legend('Training errors','Testing errors','Fontsize',15)
+grid on
+hold off
 
 %%
 
-% Plotting generalization errors for every ANN model for each fold
-
-figure
-for i = 1:K
-    plot(NHiddenUnits,ANNEgens(:,i))
-    hold on
-end
-
-%%
-
-% Plotting generalization errors for every rlr model for each fold
-
-figure
-for i = 1:K
-    semilogx(lambda_tmp,lineRegEgens(i,:))
-    hold on
-end
-
-%%
+c = cool(10);
 
 figure
 for k = 1:10
-    plot(NHiddenUnits,Error_train2_ANN(:,k),'-o','Color','blue')
+    subplot(2,1,1)
+    plot(NHiddenUnits,Error_test_gen_ANN(k,:),'-o','Color',c(k,:))
+    title('Testing errors','Fontsize',15)
+    grid on
     hold on
-    plot(NHiddenUnits,Error_test2_ANN(:,k),'-o','Color','red')
+    subplot(2,1,2)
+    plot(NHiddenUnits,Error_train_gen_ANN(k,:),'-o','Color',c(k,:))
+    title('Training errors','Fontsize',15)
+    grid on
+    hold on
 end
-legend('Training error','Test error')
+hold off
 
 %%
 
+c = cool(14);
+
 figure
-for k = 1:K
-    semilogx(lambda_tmp,Error_train2(:,k),'-o','Color','blue')
+for k = 1:10
+    subplot(2,1,1)
+    plot(lambda_tmp,Error_test_gen_rlr(k,:),'-o','Color',c(k,:))
+    title('Testing errors','Fontsize',15)
+    grid on
     hold on
-    semilogx(lambda_tmp,Error_test2(:,k),'-o','Color','red')
+    subplot(2,1,2)
+    plot(lambda_tmp,Error_train_gen_rlr(k,:),'-o','Color',c(k,:))
+    title('Training errors','Fontsize',15)
+    grid on
+    hold on
 end
-legend('Training error','Test error')
+hold off
 
 
 
